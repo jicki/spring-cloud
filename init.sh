8239565ee4c0fbae44b6d6ecdac151b0fb4d2ffc
jicki <jicki@users.noreply.github.com>
2017-10-29 00:00:57 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 mkdir -p ./data/mysql/{binlog,data,logs,conf}
+mkdir -p ./data/redis/data
 chown -R systemd-bus-proxy:input ./data/mysql/*
-chown -R 100:100 ./data/redis/*
+chown -R 100:100 ./data/redis
