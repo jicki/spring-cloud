158f9dab9c08fde5d0f0222931d2e5d71a74fe8e
jicki <jicki@users.noreply.github.com>
2017-10-29 00:09:46 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+mkidr -p ./data/rabbitmq
 mkdir -p ./data/mysql/{binlog,data,logs,conf}
 mkdir -p ./data/redis/data
 chown -R systemd-bus-proxy:input ./data/mysql/*
 chown -R 100:100 ./data/redis
+chmod -R 100:100 ./data/rabbitmq
