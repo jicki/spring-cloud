3d0e597fe561cc21568cd576bbc3d4dae67d870b
jicki <jicki@users.noreply.github.com>
2017-11-23 09:52:48 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-mkidr -p ./data/rabbitmq
+mkdir -p ./data/rabbitmq
 mkdir -p ./data/mysql/{binlog,data,logs,conf}
 mkdir -p ./data/redis/data
-mkdir -p ./data/rabbitmq
 chown -R 999:999 ./data/mysql/*
 chown -R 100:100 ./data/redis
 chown -R 100:100 ./data/rabbitmq
